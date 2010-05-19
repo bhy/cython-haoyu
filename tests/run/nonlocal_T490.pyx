c77d6935f4070845c05816cf6bd98700346d93f1
Haoyu Bai <baihaoyu@gmail.com>
2010-05-19 16:56:45 +0800

    disable type inference for inner function so nonlocal variable will not get a infered type that differ with the closure entry

--- tests/run/nonlocal_T490.pyx
+++ tests/run/nonlocal_T490.pyx
@@ -13,3 +13,17 @@ def testNonlocal1():
         print(x)
         print(y)
     f()
+
+def testNonlocal2():
+    """
+    >>> testNonlocal1() 
+    1
+    2
+    """
+    # 'nonlocal' NAME (',' NAME)*
+    x = 0
+    def f():
+        nonlocal x
+        x=1
+        print(x)
+    f()
