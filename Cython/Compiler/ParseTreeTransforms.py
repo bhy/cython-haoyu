a7b01e462ed8229da2753a491e3eb40413946d80
Haoyu Bai <baihaoyu@gmail.com>
2010-06-07 23:43:50 +0800

    add pos in template substitute

--- Cython/Compiler/ParseTreeTransforms.py
+++ Cython/Compiler/ParseTreeTransforms.py
@@ -856,7 +856,7 @@ class ExceptTransform(CythonTransform, SkipDeclarations):
         new_body = self.template.substitute({
                 u'BODY': node.body,
                 u'EXC': node.target,
-            })
+            }, pos=node.pos)
         node.body = new_body
         self.visitchildren(node)
         return node
