3c1bd920ecf3d739d0f6ef6da2145e8d8db0a79c
Haoyu Bai <baihaoyu@gmail.com>
2010-06-28 23:11:34 +0800

    clean up relative import test case

--- tests/run/relativeimport_T542.pyx
+++ tests/run/relativeimport_T542.pyx
@@ -1,14 +1,3 @@
-#def build_package():
-    #with open('__init__.py', 'w') as f:
-        #f.write('\n')
-    #with open('foo.py', 'w') as f:
-        #f.write("""
-#baz = 1
-#def bar():
-    #return 'bar'
-        #""")
-
-#build_package()
 from distutils import cmd, core, version
 __package__='distutils' # fool Python we are in distutils
 
