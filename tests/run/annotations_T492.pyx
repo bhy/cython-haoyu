b1714836cf31ff462e187244e74524c1d2225c39
Haoyu Bai <baihaoyu@gmail.com>
2010-08-07 14:06:54 +0800

    more tests

---
+++ tests/run/annotations_T492.pyx
@@ -0,0 +1,66 @@
+import cython
+
+
+def test_double1(x: cython.double):
+    """
+    >>> isinstance(test_double1(1), float)
+    True
+    """
+    return x
+
+def test_double2(x: cython.double):
+    """
+    >>> test_double2('abc') #doctest: +ELLIPSIS
+    Traceback (most recent call last):
+        ...
+    TypeError: ...
+    """
+    if not cython.compiled:
+        if not isinstance(x, float):
+            raise TypeError('a float is required.')
+    return x
+
+
+@cython.cfunc
+def foo(x: cython.double):
+    return x
+
+def test_foo():
+    """
+    >>> isinstance(test_foo(), float)
+    True
+    """
+    return foo(1)
+
+MyStruct = cython.struct(x=cython.int, y=cython.double)
+@cython.cfunc
+def feed_me_struct(a: MyStruct):
+    print(a.x)
+    return a.y
+
+def test_struct():
+    """
+    >>> y = test_struct()
+    1
+    >>> isinstance(y, float)
+    True
+    """
+    a = cython.declare(MyStruct)
+    a.x = a.y = 1
+    d = dict()
+    return feed_me_struct(a)
+
+@cython.cclass
+class MyExtType(object):
+    def shout(self):
+        print("Hello!")
+        return
+
+def test_exttype(a: MyExtType):
+    """
+    >>> test_exttype(MyExtType())
+    Hello!
+    """
+    a.shout()
+    return
+
